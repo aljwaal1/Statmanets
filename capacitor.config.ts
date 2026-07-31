6db24ed6683eaf1a86c0ecde13fc313c5f71d696
aljwaal1 <yaya15112016@gmail.com>
2026-07-31 16:26:19 +0300

    Configure Capacitor Android app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.flagship.statmanets',
+  appName: 'Statmanets',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
